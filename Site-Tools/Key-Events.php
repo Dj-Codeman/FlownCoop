c0e52394f6b5cd9751afbeac6b7c5ed3161f8e7c
Darrion Whitfield <dwhitfield@ramfield.net>
2022-10-12 00:36:43 -0500

    Debugging the debuggers

--- Site-Tools/Key-Events.php
+++ Site-Tools/Key-Events.php
@@ -3,7 +3,9 @@ $array = array();
 for ($k = 0; $k < 1000; $k++) {
   for ($i = 0; $i < 1000; $i++) {
 
-    include('../server.php');
+    // include('../server.php');
+    $db = mysqli_connect('207.244.242.167', 'Client', 'Y&0E1{8u){S?', 'Nest');
+
     $UID = MD5(uniqid());
     echo $UID;
     echo "</br>";
