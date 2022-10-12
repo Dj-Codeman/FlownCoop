9f65198e7e355bd6fa0f83d62f9564bc0d35bb3b
Darrion Whitfield <dwhitfield@ramfield.net>
2022-10-12 00:58:00 -0500

    updating update script

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-curl https://nest.ramfield.net/Total.php >>/dev/null
-curl https://nest.ramfield.net/classmanagment.php >>/dev/null
-curl https://nest.ramfield.net/class_reset.php >>/dev/null
+curl -kv https://nest.ramfield.net/Total.php >>/dev/null
+curl -kv https://nest.ramfield.net/classmanagment.php >>/dev/null
+curl -kv https://nest.ramfield.net/class_reset.php >>/dev/null
